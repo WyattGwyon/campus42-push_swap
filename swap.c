5ebf552e7c751ea2ec46abeb5d408a538e6ca579
Clayton Louden <clouden@student.42madrid.com>
2025-08-06 19:25:32 +0200

    same message as before but added swap.c

--- swap.c
+++ swap.c
@@ -22,3 +22,22 @@ void	swap(t_stack_node **head)
 	(*head)->prev->next = *head;
 	*head = (*head)->prev;
 }
+
+void	sa(t_stack_node **a)
+{
+	swap(a);
+	write(1, "sa\n", 3);
+}
+
+void	sb(t_stack_node **b)
+{
+	swap(b);
+	write(1, "sb\n", 3);
+}
+
+void	ss(t_stack_node **a, t_stack_node **b)
+{
+	swap(a);
+	swap(b);
+	write(1, "sb\n", 3);
+}
